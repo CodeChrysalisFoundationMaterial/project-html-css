2c7f85368fb92b4f52adae1f02a6114c2d74994e
miku <svartkatt39@gmail.com>
2021-02-18 23:16:07 +0900

    my initial commit

---
+++ _lesson-template/script.js
@@ -0,0 +1,10 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+console.log("Welcome to Foundations!");
+
+//　演習手順
+// _lesson-templates フォルダをコピーする
+// コピーしたフォルダをhtml-css-homework という名前に変更する。
+// フォルダ html-css-homeworkの中に　styles.cssという名前のファイル（フォルダじゃないよ）を作る
+//　cssファイルを indx.htmlにリンクさせる。monarchのリンクを参考にしてね。『どこ』にファイルを配置するかも考えよう！
