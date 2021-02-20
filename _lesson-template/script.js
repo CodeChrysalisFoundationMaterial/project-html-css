2c657d3591c7d7f05e6ef07cab6be0c60616788a
miku <svartkatt39@gmail.com>
2021-02-20 12:50:42 +0900

    refactored code

--- _lesson-template/script.js
+++ _lesson-template/script.js
@@ -5,6 +5,9 @@ console.log("Welcome to Foundations!");
 
 //　演習手順
 // _lesson-templates フォルダをコピーする
+
 // コピーしたフォルダをhtml-css-homework という名前に変更する。
+
 // フォルダ html-css-homeworkの中に　styles.cssという名前のファイル（フォルダじゃないよ）を作る
+
 //　cssファイルを indx.htmlにリンクさせる。monarchのリンクを参考にしてね。『どこ』にファイルを配置するかも考えよう！
