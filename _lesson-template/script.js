f428ad75398735bc76e7792788c9c6b8da3feff7
miku <svartkatt39@gmail.com>
2021-09-11 12:30:17 +0900

    refactored code

--- _lesson-template/script.js
+++ _lesson-template/script.js
@@ -40,9 +40,20 @@ https://codepen.io/
 https://saruwakakun.com/html-css/basic/html
 https://saruwakakun.com/html-css/basic/css
 
+HTMLで同じタグにidを複数指定はだめ？するとどうなるのか？
+https://aprico-media.com/posts/4300
+
+何らかの理由で複数のidを指定したいときがあると思いますが、
+複数のidを同じタグに指定するのはコーディング規約違反になります。
+（idを複数回使うことで目に見えるエラーが起こるとは限らない）
+
+理由
+―　idが重複してしまうと、サイト内の一部機能に不具合が発生する可能性がある
+―　jQueryでid名を使用したプログラムが書かれていた場合、もしくはid名を使用したプログラムを書く場合、正常に動作しなくなる可能性が高い
+
 */
 
 const button = document.getElementsByTagName("button"); 
 console.log("button", button); 
 
-button[0].addEventListener("click", ()=>{window.alert("Hello everyone!")}); 
+button[0].addEventListener("click", ()=>{window.alert("Hello everyone! I'm Miku!!")}); 
